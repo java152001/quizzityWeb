524132fd9e56de4cc6e32765d38da0c13e6927d4
java152001 <java152001@gmail.com>
2024-02-05 16:56:43 -0700

    Added in react-query provider and first mutation

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 type Quiz = {
     rounds: number;
@@ -8,6 +10,19 @@ type Quiz = {
 }
 
 const QuizPage = () => {
+
+    const queryClient = useQueryClient();
+
+    const {mutate: submitQuiz, isLoading} = useMutation({
+        mutationFn: async(quiz: Quiz) => await axios.post('/api/quiz/create', {
+            quiz: quiz
+        }),
+        onSuccess: () => {
+            console.log('post succeeded')
+            //queryClient.invalidateQueries(['currentQuiz'])
+        },
+        onError: () => {console.log('post failed')}
+    })
 
     const {
         register,
@@ -19,7 +34,9 @@ const QuizPage = () => {
         }
     })
 
-    const onSubmit: SubmitHandler<Quiz> = (data) => console.log(data);
+    const onSubmit: SubmitHandler<Quiz> = (data) => {
+        submitQuiz(data);
+    };
 
     return (
         <div className="max-w-xl mx-auto py-12 md:max-w-4xl">
@@ -29,12 +46,15 @@ const QuizPage = () => {
                 <form className='grid grid-cols-1 gap-6' onSubmit={handleSubmit(onSubmit)}>
                     <label className='block'>
                         <span className='text-gray-700'>Number of Rounds</span>
-                        <input className='mt-1 block w-full' placeholder='number of rounds' type='number'></input>
+                        <input className='mt-1 block w-full' {...register('rounds')} placeholder='number of rounds' type='number'></input>
                     </label>
                     <label className='block'>
                         <span className='text-gray-700'>Date of Quiz</span>
-                        <input className='mt-1 block w-full' type='date'></input>
+                        <input className='mt-1 block w-full' {...register('date')} type='date'></input>
                     </label>
+                    <button type='submit' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        Submit
+                    </button>
                 </form>
             </div>
         </div>
